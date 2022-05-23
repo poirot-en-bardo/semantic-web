d4d29f10af5adf19cdf0e2c482d1d3577528de7a
poirot-en-bardo <balica.eliza@gmail.com>
2022-05-23 20:55:20 +0300

    Project setup

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Da</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet"/>
+</head>
+<body>
+
+
+<img src="autobar.jpeg" height="430" width="800">
+
+</body>
+</html>
+
